2e03851140e5c552c7d44cc54e154e769a47a5f1
khmammed <khmammed@gmail.com>
2025-07-23 16:17:06 +0100

    Add sidebar view registration and tree data provider for SprintDesk panels

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,34 @@ import { registerViewTasksCommand } from "./commands/viewTasks";
 import { registerViewBacklogsCommand } from "./commands/viewBacklogs";
 import { addMultipleTasksCommand } from "./commands/addMultipleTasksCommand";
 import { registerViewEpicsCommand } from "./commands/viewEpics";
+import { getWebviewContent } from "./webview/getWebviewContent";
+import { SprintsTreeDataProvider } from './sidebar/SprintsTreeDataProvider';
+
+const SIDEBAR_VIEW_IDS = [
+  "sprintdesk-sprints",
+  "sprintdesk-backlogs",
+  "sprintdesk-epics",
+  "sprintdesk-tasks"
+];
+
+class SprintDeskSidebarProvider implements vscode.WebviewViewProvider {
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.joinPath(this.context.extensionUri, 'dist', 'webview'),
+        this.context.extensionUri
+      ]
+    };
+    webviewView.webview.html = getWebviewContent(this.context, webviewView.webview);
+  }
+}
 
 export async function activate(context: vscode.ExtensionContext) {
   // Register your existing commands
@@ -12,6 +40,24 @@ export async function activate(context: vscode.ExtensionContext) {
   registerViewBacklogsCommand(context);
   addMultipleTasksCommand(context);
   registerViewEpicsCommand(context);
+
+  // Register a WebviewViewProvider for each sidebar panel
+  for (const viewId of SIDEBAR_VIEW_IDS) {
+    context.subscriptions.push(
+      vscode.window.registerWebviewViewProvider(
+        viewId,
+        new SprintDeskSidebarProvider(context)
+      )
+    );
+  }
+
+  // Register the Sprints tree data provider for the Sprints view
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider(
+      'sprintdesk-sprints',
+      new SprintsTreeDataProvider()
+    )
+  );
 
   // === Auto-copy .SprintDesk template on activation ===
   const workspaceFolders = vscode.workspace.workspaceFolders;
